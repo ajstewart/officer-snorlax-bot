a2e5d76af8c5d2716879ee087abd21f96d09e7d2
Adam Stewart <adam.stewart73@gmail.com>
2023-10-07 09:41:31 +0100

    Bumped to dev version

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 from cogs.utils.checks import check_admin
 from cogs.utils.utils import get_logger, get_prefix
 
-__version__ = "1.1.2"
+__version__ = "1.2.0-dev"
 DOCS_URL = "placeholder"
 
 
